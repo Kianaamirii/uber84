c22b5522d4002bac8a407126ede2b5d459a72e8f
Kiana <150376983+Kianaamirii@users.noreply.github.com>
2025-12-19 00:14:22 +0330

    new files from stage2
    project changes of uber project

---
+++ uberproject/models/passenger.py
@@ -0,0 +1,8 @@
+
+
+    
+class Passenger:
+    def __init__(self, passenger_id, name, location):
+        self.passenger_id = passenger_id
+        self.name = name
+        self.location = location
---
+++ uberproject/utils/distance.py
@@ -0,0 +1,5 @@
+import math
+
+
+def distance(loc1, loc2):
+   return math.sqrt((loc1[0]-loc2[0])**2 + (loc1[1]-loc2[1])**2)
